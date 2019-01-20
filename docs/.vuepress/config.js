892361947ae43915acb7dd68a2f8810936ce6f82
jiaming743 <743192023@qq.com>
2019-01-20 19:02:08 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,63 @@
+module.exports = {
+  title: 'CRender_Guide',
+  description: 'Just playing around',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
+  host: '192.168.10.150',
+  port: 5002,
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: 'CRender',
+      description: '基于Canvas的矢量图形渲染插件'
+    },
+    '/EN/': {
+      lang: 'en-US',
+      title: 'CRender',
+      description: 'Canvas-based vector graphics rendering plugin'
+    }
+  },
+  themeConfig: {
+    locales: {
+      '/': {
+        selectText: '选择语言',
+        label: '简体中文',
+        nav: [
+          {
+            text: '指南',
+            link: '/guide/'
+          },
+          {
+            text: 'GitHub',
+            link: 'https://github.com/jiaming743/CRender_Guide'
+          }
+        ],
+        sidebar: {
+          '/guide/': [
+            ''
+          ]
+        }
+      },
+      '/EN/': {
+        selectText: 'Languages',
+        label: 'English',
+        nav: [
+          {
+            text: 'Guide',
+            link: '/guide/'
+          },
+          {
+            text: 'GitHub',
+            link: 'https://github.com/jiaming743/CRender_Guide'
+          }
+        ],
+        sidebar: {
+          '/EN/guide/': [
+            ''
+          ]
+        }
+      }
+    }
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: ./favicon.ico
+actionText: 快速开始 →
+actionLink: /guide/
+features:
+- title: 简单易用
+  details: 内置常用动效曲线，并提供方便的动效曲线绘制工具
+- title: 体积轻量
+  details: 未压缩体积不足10K
+- title: 灵动多变
+  details: 通过绘制工具可以轻易绘制出多变的动效曲线，只有想不到没有绘不出
+footer: MIT Licensed | Copyright © 2018-present JiaMing
+---
