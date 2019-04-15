c2f9bfc94b6509852b04be7ef8bd3e63e9b42dc6
jiaming743 <743192023@qq.com>
2019-04-15 10:39:15 +0800

    add note

--- docs/CRender/lib/canvas.js
+++ docs/CRender/lib/canvas.js
@@ -1,3 +1,11 @@
+/**
+ * @description Draw a polyline path
+ * @param {Object} ctx        Canvas 2d context
+ * @param {Array} points      The points that makes up a polyline
+ * @param {Boolean} beginPath Whether to execute beginPath
+ * @param {Boolean} closePath Whether to execute closePath
+ * @return {Undefined} Void
+ */
 export function drawPolylinePath (ctx, points, beginPath = false, closePath = false) {
   if (!ctx || points.length < 2) return false
 
@@ -9,6 +17,15 @@ export function drawPolylinePath (ctx, points, beginPath = false, closePath = fa
   if (closePath) ctx.closePath()
 }
 
+/**
+ * @description Draw a bezier curve path
+ * @param {Object} ctx        Canvas 2d context
+ * @param {Array} points      The points that makes up a bezier curve
+ * @param {Array} moveTo      The point need to excute moveTo
+ * @param {Boolean} beginPath Whether to execute beginPath
+ * @param {Boolean} closePath Whether to execute closePath
+ * @return {Undefined} Void
+ */
 export function drawBezierCurvePath (ctx, points, moveTo = false, beginPath = false, closePath = false) {
   if (!ctx || !points) return false
 
